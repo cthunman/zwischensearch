e81d37d0e1785c28071d238f56ee158a9b2fe388
Carl <thunman@gmail.com>
2013-08-12 20:41:20 -0400

    first sample solr commit program

--- node/solr.js
+++ node/solr.js
@@ -1,2 +1,20 @@
 
 var solrClient = require('solr-client');
+var mongo = require('./mongo');
+var uuid = require('uuid');
+
+// createClient(host, port, core, path)
+var client = solrClient.createClient('127.0.0.1', 8080, '', '/solr');
+client.autoCommit = true;
+
+// Add a new document
+var obj = { };
+obj["id"] = uuid.v4();
+
+client.add(obj, function(err, obj) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Solr response:' + obj);
+	}
+});
